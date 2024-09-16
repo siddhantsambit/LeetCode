bb8719e6fccd67169065eb628c46815ae7950b08
siddhantsambit <siddhantsambit@gmail.com>
2024-09-16 23:34:19 +0530

    LeetCode-539 MinimumTimeDifference Addition of optimized way

--- src/main/java/com/leetcode/problems/MinimumTimeDifference.java
+++ src/main/java/com/leetcode/problems/MinimumTimeDifference.java
@@ -9,9 +9,6 @@ public class MinimumTimeDifference {
 
     /*
     * LeetCode-539 MinimumTimeDifference
-    * Time Complexity : O(nlogn), n is size of input array
-    * Space Complexity : O(n)
-    * Later will look for optimize solution
     */
     public static void main(String[] args) {
         MinimumTimeDifference minimumTimeDifference = new MinimumTimeDifference();
@@ -20,14 +17,21 @@ public class MinimumTimeDifference {
         timePoints = Arrays.asList("00:00","23:59","00:00");
         System.out.println(minimumTimeDifference.findMinDifference(timePoints));
 
+        timePoints = new ArrayList<>(Arrays.asList("23:59","00:00"));
+        System.out.println(minimumTimeDifference.findMinDifferenceOptimized(timePoints));
+        timePoints = Arrays.asList("00:00","23:59","00:00");
+        System.out.println(minimumTimeDifference.findMinDifferenceOptimized(timePoints));
+
     }
 
+    /*
+     * Time Complexity : O(nlogn), n is size of input array
+     * Space Complexity : O(n)
+     */
     public int findMinDifference(List<String> timePoints) {
         List<Integer> timePointsInMins = new ArrayList<>();
         timePoints.forEach(timePoint -> {
-            int hour = Integer.parseInt(timePoint.substring(0,2));
-            int minutes = Integer.parseInt(timePoint.substring(3));
-            timePointsInMins.add(hour * 60 + minutes);
+            timePointsInMins.add(convertTimePointIntoMinutes(timePoint));
         });
         Collections.sort(timePointsInMins);
         int res = Integer.MAX_VALUE;
@@ -36,5 +40,33 @@ public class MinimumTimeDifference {
         }
         return Math.min(res, (24 * 60 - timePointsInMins.get(timePointsInMins.size()-1)
                 + timePointsInMins.get(0)));
+    }
+
+    /*
+     * Time Complexity : O(n), n is size of input array
+     * Space Complexity : O(1440)
+     */
+    public int findMinDifferenceOptimized(List<String> timePoints) {
+        boolean visited[] = new boolean[24*60];
+        for(String timePoint : timePoints) {
+            int min = convertTimePointIntoMinutes(timePoint);
+            if (visited[min]) return 0;
+            visited[min] = true;
+        }
+        int first = Integer.MAX_VALUE, prev = Integer.MAX_VALUE, res = Integer.MAX_VALUE;
+        for(int i=0; i < 24 * 60; i++) {
+            if(visited[i]) {
+                if (first == Integer.MAX_VALUE) first = i;
+                else res = Math.min(res, i - prev);
+                prev = i;
+            }
+        }
+        return Math.min(res, 24 * 60 - prev + first);
+    }
+
+    private int convertTimePointIntoMinutes(String timePoint) {
+        int hour = Integer.parseInt(timePoint.substring(0,2));
+        int minutes = Integer.parseInt(timePoint.substring(3));
+        return (hour * 60 + minutes);
     }
 }
